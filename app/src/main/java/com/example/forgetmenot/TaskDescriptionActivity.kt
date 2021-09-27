146178d14bce525602caffb2b48d5261a5943234
Ayana Bando <ayanaflax@yahoo.com>
2021-09-27 12:15:32 -0400

    refactor code in classes and layout files

--- app/src/main/java/com/example/forgetmenot/TaskDescriptionActivity.kt
+++ app/src/main/java/com/example/forgetmenot/TaskDescriptionActivity.kt
@@ -1,60 +1,35 @@
 package com.example.forgetmenot
 
-import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.example.forgetmenot.R.layout.activity_task_description
-import java.lang.reflect.Array.get
 
-
-private val Nothing?.text: Any
-    get() {}
+const val EXTRA_TASK_DESCRIPTION = "task"
 
 class TaskDescriptionActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(activity_task_description)
-        private val kotlin.Nothing?.text: Any
-        get() {}
-        
+        setContentView(R.layout.activity_task_description)
+    }
 
-        val companion
-        val companion1 = companion object {
-            val EXTRA_TASK_DESCRIPTION = "task"
-        }
-
-        // 2
-        @SuppressLint("MissingSuperCall")
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            super.onCreate(savedInstanceState)
-            super.onCreate(savedInstanceState)
-            setContentView(activity_task_description)
-        }
-
-        // 3
-        fun doneClicked(view: View) {
-
-        }
+    // 3
+    fun doneClicked(view: View) {
         // 1
-        val descriptionText = null
+        val descriptionText = findViewById<EditText>(R.id.descriptionText)
         val taskDescription = descriptionText.text.toString()
 
         if (!taskDescription.isEmpty()) {
             // 2
             val result = Intent()
-            val EXTRA_TASK_DESCRIPTION
             result.putExtra(EXTRA_TASK_DESCRIPTION, taskDescription)
             setResult(Activity.RESULT_OK, result)
         } else {
             // 3
             setResult(Activity.RESULT_CANCELED)
-        }
-
-// 4
+        }// 4
         finish()
     }
 }
